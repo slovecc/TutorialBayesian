0f01f6600728b351b0da0617118d07f62478ee6a
slovecc <lovecchio.salvo@gmail.com>
2022-02-01 01:05:05 +0100

    new build

---
+++ bayesian-course/tutorial/_build/jupyter_execute/Examples for ecommerce.py
@@ -0,0 +1,22 @@
+# Examples for ecommerce  <a class="anchor" id="chapter5"></a>
+
+
+In this section, we collect all the possible application of Bayesian statistic for ecommerce:
+
+- Example Searches with Clicks
+- Example Performance of searches per platform
+- Example Ratings
+
+
+
+
+
+```{toctree}
+:hidden:
+:titlesonly:
+
+
+chaptExamples/Example Searches with Clicks
+chaptExamples/Example Performance of searches per platform
+chaptExamples/Example Ratings
+```
